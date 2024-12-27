6eb06b115739ec5d0f8635a761ad0d744f34eb22
box16 <inoinobox16@gmail.com>
2024-12-27 17:20:27 +0900

    簡易的な実装

---
+++ ApplicationService/dto_device.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass(frozen=True)
+class Device:
+    id: str
+    name: str
+    type: str
+
+
+@dataclass(frozen=True)
+class DeviceList:
+    columns: Tuple[str, ...]
+    devices: Tuple[Device]
