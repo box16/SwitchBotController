cce6e0ebab4c973747e8ee46a882b1b0cbfd5888
box16 <inoinobox16@gmail.com>
2024-12-29 17:12:07 +0900

    POST実装

--- Infra/api_gateway.py
+++ Infra/api_gateway.py
@@ -1,9 +1,51 @@
+import time
+import hashlib
+import hmac
+import base64
+import uuid
+import os
+import json
+import requests
 from Domain.api_gateway import ISwitchBotGateway
 
 
 class SwitchBotGateway(ISwitchBotGateway):
+    def _create_header(self):
+        token = os.getenv("SWITCHBOT_TOKEN")
+        time_stamp = int(round(time.time() * 1000))
+        nonce = uuid.uuid4()
+        message = bytes(f"{token}{time_stamp}{nonce}", "utf-8")
+
+        secret_key = os.getenv("SWITCHBOT_SECRET_KEY")
+        secret_key = bytes(secret_key, "utf-8")
+        sign = base64.b64encode(
+            hmac.new(secret_key, msg=message, digestmod=hashlib.sha256).digest()
+        )
+
+        return {
+            "Authorization": token,
+            "Content-Type": "application/json",
+            "charset": "utf8",
+            "t": str(time_stamp),
+            "sign": str(sign, "utf-8"),
+            "nonce": str(nonce),
+        }
+
     def send_toggle_switch(self, device_id):
-        pass
+        header = self._create_header()
+        data = json.dumps(
+            {
+                "commandType": "command",
+                "command": "toggle",
+                "parameter": "default",
+            }
+        )
+
+        requests.post(
+            f"https://api.switch-bot.com/v1.1/devices/{device_id}/commands",
+            headers=header,
+            data=data,
+        )
 
 
 class FakeSwitchBotGateway(ISwitchBotGateway):
--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from ApplicationService.device_app_service import DeviceAppService
 from ApplicationService.dto_device import DeviceList
 from Infra.device_repository import DeviceRepository
+from Infra.api_gateway import SwitchBotGateway
 
 app = Flask(__name__)
+device_app_service = DeviceAppService(DeviceRepository(), SwitchBotGateway())
 
 
 @app.route("/")
 def index():
-    device_app_service = DeviceAppService(DeviceRepository())
     device_list: DeviceList = device_app_service.get_all()
     return render_template(
         "index.html", columns=device_list.columns, devices=device_list.devices
