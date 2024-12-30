fcec78b2948fb7be985f6f2b87a00b21879d322a
box16 <inoinobox16@gmail.com>
2024-12-30 21:21:28 +0900

    異常系をパスする仮実装

--- ApplicationService/Group/group_app_service.py
+++ ApplicationService/Group/group_app_service.py
@@ -1,6 +1,10 @@
 from Domain.Group.group_repository import IGroupRepository
 from Domain.api_gateway import ISwitchBotGateway
-from utility.exception import DeviceNotFound
+from utility.exception import (
+    DeviceNotFound,
+    CreateGroupWithoutDevice,
+    CreateGroupWithoutname,
+)
 from typing import List, Tuple
 from ApplicationService.Group.dto_group import Group as DGroup
 from Domain.Group.group import Group
@@ -12,6 +16,12 @@ class GroupAppService:
         self.api_gateway = api_gateway
 
     def create_group(self, device_id_list: List[str], name: str):
+        # TODO:ここのロジックはドメイン層に移す
+        if not device_id_list:
+            raise CreateGroupWithoutDevice
+        if not name:
+            raise CreateGroupWithoutname
+
         try:
             self.db.add(device_id_list, name)
         except DeviceNotFound as e:
