8a592697322bfcadb6290ee000513056fb6140a4
box16 <inoinobox16@gmail.com>
2024-12-30 17:18:43 +0900

    DeviceRepositoryの修正

--- Infra/device_repository.py
+++ Infra/device_repository.py
@@ -10,6 +10,20 @@ class DeviceRepository(IDeviceRepository):
         self_dir = os.path.dirname(os.path.abspath(__file__))
         self.db_path = os.path.join(self_dir, "devices.db")
 
+        connection = sqlite3.connect(self.db_path)
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+                CREATE TABLE IF NOT EXISTS devices (
+                    id TEXT PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    type TEXT NOT NULL,
+                    enable_cloud_service BOOLEAN NOT NULL,
+                    hub_device_id TEXT NOT NULL
+                )
+            """
+        )
+
     def get_all(self) -> Tuple[Device]:
         connection = sqlite3.connect(self.db_path)
         cursor = connection.cursor()
@@ -32,7 +46,9 @@ class DeviceRepository(IDeviceRepository):
         # executeはパラメーターにシーケンスを要求するため、引数1つでもこのように指定する
         cursor.execute("SELECT EXISTS(SELECT 1 FROM devices WHERE id=?)", (device_id,))
         # fetchone() は (0,) または (1,) のタプルを返すので、添字 [0] を取り出して bool にする
-        return bool(cursor.fetchone()[0])
+        is_exist = bool(cursor.fetchone()[0])
+        connection.close()
+        return is_exist
 
 
 class InMemoryDeviceRepository(IDeviceRepository):
